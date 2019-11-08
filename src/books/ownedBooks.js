cf53430b4391092943d71bb2ff38f954d3508541
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-11-07 22:15:01 -0500

    made ./ownedBooks.js into a class component

--- src/books/ownedBooks.js
+++ src/books/ownedBooks.js
@@ -2,11 +2,14 @@ import React from 'react'
 import './books.css'
 import Book from './Book.js'
 
-const OwnedBooks = () => {
-  return (
-    <Book />
-    // {/* <h2>books will go here</h2> */}
-  )
+class OwnedBooks extends React.Component {
+
+  render() {
+    return (
+      <Book />
+      // {/* <h2>books will go here</h2> */}
+    )
+  }
 }
 
 export default OwnedBooks
