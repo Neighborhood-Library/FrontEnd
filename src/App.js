590e29a4583ff92f7aa3e4f0cc76b82983ca0d8d
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-11-11 22:16:52 -0500

    added redux and react redux, added src/actions/ and src/reducers/ along with files. getting started on building out redux files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css'
 import Header from './Header.js'
 import OwnedBooks from './books/ownedBooks.js'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <OwnedBooks />
-    </div>
-  )
-}
+import { connect } from 'react-redux'
+class App extends Component {
 
-export default App
+  render() {
+    return (
+        <div className="App">
+          <Header />
+          <OwnedBooks />
+        </div>
+      )
+  }
+
+}
+const mapStateToProps = state => ({
+  books: state.books,
+  user: state.user
+})
+const mapActionsToProps = {
+
+}
+export default connect(mapStateToProps)(App)
--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { createStore } from 'redux'
+import { combineReducers, Provider } from 'react-redux'
+import bookReducer from './reducers/bookReducer.js'
+import userReducer from './reducers/userReducer.js'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const allReducers = combineReducers({
+  books: bookReducer,
+  user: userReducer
+})
+const store = createStore(allReducers, {
+  books: [{
+    title: 'The Outsiders',
+    user: 'Carlos'
+  }]
+})
+const action = {
+  type: 'changeState',
+  payload: {
+    newState: 'New State'
+  }
+}
+store.dispatch(action)
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
