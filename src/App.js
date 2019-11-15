d6d3f9009930ff0b4edf49b92b0695e86373a962
cpscott1 <thacam18@gmail.com>
2019-11-14 23:45:07 -0600

    fixed issue with landing page causing horizontal scrollbar.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import HomeDashboard from './components/homeDashboard/HomeDashboard';
 import LoginForm from './components/login/LoginForm';
 import RegisterForm from './components/registration/RegisterForm';
+import { LandingPage } from './components/landingPage/LandingPage';
 
 function App() {
   return (
     <div className='App'>
+    <header>
+      <div className="logo">logo</div>
+      <nav className="app-nav">
+        <Link to="/about">About</Link>
+        <Link to="/shelf">Shelf</Link>
+        <Link className="login-btn" to="/login">Login</Link>
+      </nav>
+    </header>
       <Switch>
-        <Route exact path='/' component={HomeDashboard} />
+
+        <Route exact path='/' component={LandingPage} />
         <Route path='/login' component={LoginForm} />
         <Route path='/register' component={RegisterForm} />
       </Switch>
