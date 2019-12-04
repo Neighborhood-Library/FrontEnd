3355514ba039f6d359590879d943b756d463d05f
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-12-03 20:50:53 -0800

    search filter

--- src/components/borrowerDashboard/BorrowerDashboard.js
+++ src/components/borrowerDashboard/BorrowerDashboard.js
@@ -1,5 +1,47 @@
 import React from 'react';
 import './BorrowerDashboard.css';
+import FilterResults from "react-filter-search";
+
+class App extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      data: [ ],
+      value: ' '
+    };
+  }
+  componentWillMount() {
+    fetch('http://https://neighborhood-library-labspt5.netlify.com/borrowers')
+      .then(response => response.json())
+      .then(json => this.setState({ data: json }));
+  }
+  handleChange = event => {
+    const { value } = event.target;
+    this.setState({ value });
+  };
+  render() {
+    const { data, value } = this.state;
+    return (
+      <div>
+        <input type= "text" value={value} onChange={this.handleChange} />
+        <SearchResults 
+          value={value}
+          data={data}
+          renderResults={results => (
+            <div>
+              {results.map(el => (
+                <div>
+                  <span>{el.name}</span>
+                  <span>{el.title}</span>
+                </div>
+            ))}
+            </div>
+          )}
+        />
+      </div>
+    )
+  }
+}
 
 export default class BorrowerDashboard extends React.Component {
   render() {
