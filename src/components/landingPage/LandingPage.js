8f202c74516751184ad0cf750bc8d07e23b0efd4
cpscott1 <thacam18@gmail.com>
2019-12-04 15:43:04 -0600

    add some responsiveness to landing page.

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,22 +1,7 @@
 import React from 'react';
-import './landing.css';
+import './landing.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookOpen, faUserFriends } from '@fortawesome/free-solid-svg-icons';
-
-export const backgroundStyle = {
-  backgroundPosition: 'center',
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat',
-  minHeight: '100%',
-  minWidth: '1024px',
-  width: '100%',
-  height: 'auto',
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  border: '1px solid black',
-  zIndex: 97
-}
 
 export const LandingPage = () => {
   return (
