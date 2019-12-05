f30bf1f58850706afd83b4cd689963e551fa1994
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-12-04 21:58:37 -0500

    removed homedashboard from app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
-import HomeDashboard from './components/homeDashboard/HomeDashboard';
+// import HomeDashboard from './components/homeDashboard/HomeDashboard';
 import LoginForm from './components/login/LoginForm';
 import RegisterForm from './components/registration/RegisterForm';
 import { LandingPage } from './components/landingPage/LandingPage';
