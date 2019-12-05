a3e1c900eacf1840cfa5919ff48d36c33493d9d3
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-12-04 21:44:18 -0500

    removed serviceworker

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import thunk from 'redux-thunk';
 import App from './App';
 import './index.css';
 // import rootReducer from './reducers';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 // include rootReducer, after reducers created
 const store = createStore(applyMiddleware(thunk, logger));
@@ -25,4 +25,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
