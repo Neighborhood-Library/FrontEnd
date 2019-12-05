013e0104746e0bfafe72e3096d1414a62775fadb
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-12-04 18:53:43 -0800

    added borrow to the route path

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomeDashboard from './components/homeDashboard/HomeDashboard';
 import LoginForm from './components/login/LoginForm';
 import RegisterForm from './components/registration/RegisterForm';
+import BorrowerDashboard from './components/borrowerDashboard/BorrowerDashboard';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path='/' component={HomeDashboard} />
         <Route path='/login' component={LoginForm} />
         <Route path='/register' component={RegisterForm} />
+        <Route path='/borrow' component={BorrowerDashboard} />
       </Switch>
     </div>
   );
