ad0e55b2198d2b7d7e531ddb56b3493356da8321
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-12-10 19:10:10 -0800

    added missing dispatch

--- src/components/borrowerDashboard/actions/index.js
+++ src/components/borrowerDashboard/actions/index.js
@@ -6,7 +6,7 @@ export const ADDING_BOOK_SUCC = "ADDING_BOOK_SUCC";
 export const ADDING_BOOK_FAIL = "ADDING_BOOK_FAIL";
 export const DELETING_BOOK = "DELETING_BOOK";
 
-export const getBooks = () => {
+export const getBooks = () => dispatch => {
     dispatch({ type: FETCH_BOOK });
     axios
         .get('https://neighborhood-library-labspt5.netlify.com/books')
