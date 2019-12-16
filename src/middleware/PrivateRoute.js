cd8110e379af99ac8acc3eef79f7127b7eec8c4f
shenry45 <shawn45henry@gmail.com>
2019-12-15 21:17:03 -0700

    privateroute testing

--- src/middleware/PrivateRoute.js
+++ src/middleware/PrivateRoute.js
@@ -42,8 +42,7 @@ class PrivateRoute extends React.Component {
   }
 
   componentWillMount = async () => {
-    const URL = process.env.REACT_APP_ENV === 'testing' ? 'http://localhost:5000/auth/current_user' : 'http://localhost:5000/auth/current_user';
-    // https://muovivlio.herokuapp.com/auth/current_user
+    const URL = process.env.REACT_APP_ENV === 'testing' ? 'http://localhost:5000/auth/current_user' : 'https://muovivlio.herokuapp.com/auth/current_user';
 
     await axios
       .get(URL, { withCredentials: true })
