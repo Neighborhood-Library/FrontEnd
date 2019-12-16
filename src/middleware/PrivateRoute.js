ede0123884ee6e7c31da6df0cf061db67de3909b
shenry45 <shawn45henry@gmail.com>
2019-12-15 21:37:30 -0700

    privateroute testing

--- src/middleware/PrivateRoute.js
+++ src/middleware/PrivateRoute.js
@@ -2,97 +2,41 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import axios from 'axios';
 
-// const PrivateRoute = async ({ component: Component, ...rest }) => {
-//   const signedIn = await axios
-//     .get('https://muovivlio.herokuapp.com/auth/current_user', {
-//       withCredentials: true
-//     })
-//     .then(res => {
-//       // return true
-//       return (<Route {...rest} render={() => <Component />} />)
-//     })
-//     .catch(err => {
-//       // return false
-//       return (<Route render={() => <Redirect to="/login" />} />)
-//     });
+class PrivateRoute extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      auth: undefined
+    }
+  }
 
-//     console.log(signedIn);
+  componentWillMount = async () => {
+    const URL = process.env.REACT_APP_ENV === 'testing' ? 'http://localhost:5000/auth/current_user' : 'https://muovivlio.herokuapp.com/auth/current_user';
 
-//     return signedIn;
+    await axios
+      .get(URL, { withCredentials: true })
+      .then(res => {
+        this.setState({ auth: true });
+        return;
+      })
+      .catch(err => {
+        this.setState({ auth: false });
+      });
+  }
 
-//   // return (
-//   //   <Route
-//   //     {...rest}
-//   //     render={() => 
-//   //       signedIn ? (
-//   //         <Component />
-//   //       ) : (
-//   //         <Redirect to="/login" />
-//   //       )}
-//   //   />
-//   // )
-// };
-
-const getUser = async () => {
-  const URL = process.env.REACT_APP_ENV === 'testing' ? 'http://localhost:5000/auth/current_user' : 'https://muovivlio.herokuapp.com/auth/current_user';
-
-  await axios
-    .get(URL, { withCredentials: true })
-    .then(res => {
-      return true;
-    })
-    .catch(err => {
-      return false;
-    });
-}
-
-console.log(getUser());
-
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={props => (
-    getUser() ? (
-      <Component {...props}/>
-    ) : (
-      <Redirect to='/login' />
+  render() {
+    return(
+      <>
+        {
+          this.state.auth ? (
+            <Route props render={() => <props.Component />} />
+          ) : (
+            <Route render={() => <Redirect to="/login" />} />
+          )
+        }
+      </>
     )
-  )}/>
-)
-
-// class PrivateRoute extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       auth: undefined
-//     }
-//   }
-
-//   componentWillMount = async () => {
-//     const URL = process.env.REACT_APP_ENV === 'testing' ? 'http://localhost:5000/auth/current_user' : 'https://muovivlio.herokuapp.com/auth/current_user';
-
-//     await axios
-//       .get(URL, { withCredentials: true })
-//       .then(res => {
-//         this.setState({ auth: true });
-//         return;
-//       })
-//       .catch(err => {
-//         this.setState({ auth: false });
-//       });
-//   }
-
-//   render() {
-//     return(
-//       <>
-//         {
-//           this.state.auth ? (
-//             <Route props render={() => <props.Component />} />
-//           ) : (
-//             <Route render={() => <Redirect to="/login" />} />
-//           )
-//         }
-//       </>
-//     )
-//   }
-// }
+  }
+}
 
 export default PrivateRoute;
