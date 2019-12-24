551f843af5d5664d30fed6063d5afd3aa521bef6
cpscott1 <thacam18gmail.com>
2019-12-23 23:34:13 -0600

    made cards for about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './about.scss';
 
 const About = () => {
   const team = [
@@ -51,21 +52,21 @@ const About = () => {
 
   return (
     <div className="page-wrap">
-      <section>
-        <h1>About Us</h1>
+      <section className='card'>
+        <h1 className="card-title">About</h1>
         <hr />
         <p>
         <b>Geia sas, aftí eínai i vivliothíki geitoniás!</b>...Sorry, Hello! This is Neighborhood Library. As people who have a Deep appreciation for printed books, we wanted to be able to speak to others about the mental adventures we go through everytime we dive back into our favorite book. When we looked around, we could never find someone else near us that either has read the same book we have or even have a care about the book that were obsessed about at the moment. We thought about it and decided that making an app that would allow book lovers of all ages and genres Finally be able to interact with each other and even be able to share their own books with others.
-        
+
         This person is seeking a way to interact with locals who love books and share books with each other.
         </p>
         <br></br>
-        <p> 
+        <p>
         Libraries are "places of <b>Freedom</b>” — <strong>Freedom</strong> to Choose, <strong>Freedom</strong> to Discover, <strong>Freedom</strong> to Question — <strong>Freedom</strong> to Borrow! In a world where very little is ever truly ‘free’, libraries are champions of <strong>Freedom</strong>." In this app, we provide a <b><em>Simple and Feature-Rich</em></b> solution for lending and borrowing books to/from fellow users.
         </p>
       </section>
-      <section>
-        <h2>Team</h2>
+      <section className='card'>
+        <h2 className="card-title">Team</h2>
         <hr />
         <div className="team-cont">
           {
