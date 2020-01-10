d7d602d9fa770c3720bb092423dda31ac2838c11
shenry45 <shawn45henry@gmail.com>
2020-01-09 22:23:41 -0700

    0 messages handling

--- src/components/chat/Messages/Messages.js
+++ src/components/chat/Messages/Messages.js
@@ -10,22 +10,28 @@ class Messages extends React.Component {
 		const user1 = this.props.transaction.borrower_id;
 		// const user2 = this.props.transaction.lender_id;
 	
-		return (
-			<ScrollToBottom className='messages'>
-				{this.props.messages.map(
-					(message, i) => {
-						return (
-							<div key={i}>
-								<Message
-									message={message}
-									id={message.sender_id === user1 ? 'user1' : 'user2'}
-								/>
-							</div>
-						)
-					}
-				)}
-			</ScrollToBottom>
-		)
+		if (this.props.messages.length > 0) {
+			return (
+				<ScrollToBottom className='messages'>
+					{
+						
+					this.props.messages.map(
+						(message, i) => {
+							return (
+								<div key={i}>
+									<Message
+										message={message}
+										id={message.sender_id === user1 ? 'user1' : 'user2'}
+									/>
+								</div>
+							)
+						}
+					)}
+				</ScrollToBottom>
+			)
+		} else {
+			return <ScrollToBottom className='messages'><p>Nothing to see.</p></ScrollToBottom>
+		}
 	}
 };
 
