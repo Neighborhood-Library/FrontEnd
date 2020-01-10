9bd1e187dea2e53acefe9f3c7146df3aa0aed436
shenry45 <shawn45henry@gmail.com>
2020-01-09 22:09:23 -0700

    transaction handling for books

--- src/components/userDashboard/Book.js
+++ src/components/userDashboard/Book.js
@@ -81,7 +81,7 @@ class Book extends React.Component {
     await Axios
       .get(`${process.env.REACT_APP_REQ_URL}/api/transaction/${user_id}&${this.props.book.google_book_id}`, {withCredentials: true})
       .then(res => {
-        if (res.data.message !== []) {
+        if (res.data.message !== undefined && res.data.message.length > 0) {
           this.setState({ transaction: res.data.message })
         } else {
           this.checkAvailable();
