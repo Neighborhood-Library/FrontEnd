3f9350406e843adccf03bfd8c0c089da899d8c0d
shenry45 <shawn45henry@gmail.com>
2020-05-26 07:34:36 -0700

    update header and landing page styling

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,5 +1,3 @@
-import { faBookOpen, faUserFriends } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CustomButton from '../customButton/CustomButton';
@@ -7,32 +5,15 @@ import './landing.scss';
 
 export const LandingPage = () => {
 	return (
-		<div className='landing-background'>
+		<div className='page-wrap'>
 			<div className='landing-content'>
 				<h1 className='cta'>
-					A New Way to <br />
-					Share Books
+					A New Way to Share Books
 				</h1>
 				<Link className='sign-up' to='/register'>
 					<CustomButton isSignUp>Sign Up</CustomButton>
 				</Link>
 			</div>
-			<footer className='landing-footer'>
-				<div className='icon-group'>
-					<FontAwesomeIcon className='landing-icon' icon={faBookOpen} />
-					<p className='landing-text'>
-						Share your own collection <br />
-						or borrow from others
-					</p>
-				</div>
-				<div className='icon-group'>
-					<FontAwesomeIcon className='landing-icon' icon={faUserFriends} />
-					<p className='landing-text'>
-						Join a vibrant community of book <br />
-						enthuisists in your local area
-					</p>
-				</div>
-			</footer>
 		</div>
 	);
 };
