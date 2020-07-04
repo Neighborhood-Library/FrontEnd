1a2b9bbdc11255b5537c9ad9085ea9d6dba95cff
shenry45 <shawn45henry@gmail.com>
2020-07-03 20:23:45 -0700

    front page UI and contact page Google maps embed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer>
-      {/* <p className="appTitle">Share the Experience of Your Books With Others</p> */}
+      <nav className="app-nav">
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link>Shelf</Link>
+        <Link>Books</Link>
+      </nav>
+      <p>MuoVivlio. All rights reserved.</p>
     </footer>
   )
 };
--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -8,10 +8,10 @@ export const LandingPage = () => {
 			<section id="hero">
 				<div>
 					<h1 className='cta'>
-						Explore your local book jungle
+						Explore your local book jungle!
 					</h1>
 					<p>
-						Find the books neighbors are allowing others to borrow.
+						Discover new book collections from your neighbors and community.
 					</p>
 					<Link className='sign-up' to='/register'>
 						<CustomButton isSignUp>Sign Up</CustomButton>
@@ -19,8 +19,10 @@ export const LandingPage = () => {
 				</div>
 				<img src="img/reading.svg" alt="reading book" />
 			</section>
-			<section>
-
+			<section id="milestones">
+				<h4>1,000,000+ books</h4>
+				<h4>1,500+ connections</h4>
+				<h4>No signup fees!</h4>
 			</section>
 			<section id="testimonials">
 				<div className="heading">
@@ -32,12 +34,12 @@ export const LandingPage = () => {
 					<p className="test-author">Sarah Love</p>
 				</div>
 				<div className="test-cont">
-					<p className="test-quote">I love sharing my books with my community!</p>
-					<p className="test-author">Sarah Love</p>
+					<p className="test-quote">Some great finds I never would have found otherwise...</p>
+					<p className="test-author">Derrick Jay</p>
 				</div>
 				<div className="test-cont">
-					<p className="test-quote">I love sharing my books with my community!</p>
-					<p className="test-author">Sarah Love</p>
+					<p className="test-quote">Absolutely love this!</p>
+					<p className="test-author">Geri Honor</p>
 				</div>
 			</section>
 		</div>
